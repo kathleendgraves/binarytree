28fb508ba1a5b201b1fe79af3b02d85d8c5c37d4
LAPTOP\Kathleen <kathleendgraves@gmail.com>
2017-04-17 20:38:47 -0700

    First commit: A program that takes a string, builds it into a binary tree, and then prints out the binary tree in level order. TODO: Give the user some indication the string isn't just spit back out, clean up code/comments, testing

---
+++ PrintBinaryTree/BinaryTree.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+
+namespace PrintBinaryTree
+{
+    class BinaryTree
+    {
+        private int _data;
+        private BinaryTree _left;
+        private BinaryTree _right;
+
+        public BinaryTree(int data)
+        {
+            _data = data;
+        }
+
+        public int Data
+        {
+            get
+            {
+                return _data;
+            }
+        }
+
+        public BinaryTree Left
+        {
+            get
+            {
+                return _left;
+            }
+            set
+            {
+                _left = value;
+            }
+        }
+
+        public BinaryTree Right
+        {
+            get
+            {
+                return _right;
+            }
+            set
+            {
+                _right = value;
+            }
+        }
+
+        // Prints out the binary tree in comma separated level order
+        public string toString()
+        {
+            // start with root node
+            BinaryTree root = this;
+            Queue<BinaryTree> children = new Queue<BinaryTree>();
+            children.Enqueue(root);
+
+            return formatForString(children);
+        }
+
+        private string formatForString(Queue<BinaryTree> nodes)
+        {
+            string output = "";
+            Queue<BinaryTree> children = new Queue<BinaryTree>();
+
+            // Iterate through each item in nodes
+            foreach (BinaryTree node in nodes)
+            {
+                // Add current node's value to output
+                output += node._data + ", ";
+
+                // Add current node's children to list if they exist
+                if (node._left != null)
+                {
+                    children.Enqueue(node._left);
+                }
+
+                if (node._right != null)
+                {
+                    children.Enqueue(node._right);
+                }
+            }
+
+            // Call again on children if there are any
+            if (children.Count > 0)
+            {
+                output += formatForString(children);
+            }
+
+            return output;
+        }
+    }
+}
--- PrintBinaryTree/Program.cs
+++ PrintBinaryTree/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PrintBinaryTree
 {
@@ -10,6 +7,122 @@ namespace PrintBinaryTree
     {
         static void Main(string[] args)
         {
+            // Prompt user for input string
+            Console.WriteLine("Enter a comma separated list of integer values.");
+            string input = Console.ReadLine();
+
+            // Validate
+            try
+            {
+                validateInput(input);
+
+                // Build binary tree from input
+                BinaryTree tree = loadTreeFromString(input);
+
+                // Output binary tree in level order
+                Console.Write("The binary tree, in level order: ");
+                Console.WriteLine(tree.toString());
+
+                //TODO Display string as binary tree?
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            //TODO Keep window open a little longer
+
+        }
+
+        private static void validateInput(string input)
+        {
+            if (input.Length == 0)
+            {
+                throw new ArgumentException("Please enter a string.");
+            }
+            else if (!input.Contains(","))
+            {
+                throw new ArgumentException("Please separate the integer values with a comma.");
+            }
+        }
+
+        // ex: 5, 2, 6, 7, 8, 1, 9
+        private static BinaryTree loadTreeFromString(string input)
+        {
+            // Convert to queue for consumption
+            Queue<int> valueList = convertStringToQueue(input);
+
+            // Create root node
+            BinaryTree root = new BinaryTree(valueList.Dequeue());
+
+            // Create queue to be processed
+            Queue<BinaryTree> nodeList = new Queue<BinaryTree>();
+            nodeList.Enqueue(root);
+
+            // Build the binary tree
+            buildNodes(nodeList, valueList);
+
+            return root;
+        }
+        
+        // Builds the binary tree recursively using the input
+        private static void buildNodes(Queue<BinaryTree> nodeList, Queue<int> valueList)
+        {
+            Queue<BinaryTree> childrenList = new Queue<BinaryTree>();
+
+            foreach (BinaryTree node in nodeList)
+            {
+                BinaryTree left, right;
+                if (valueList.Count > 0)
+                {
+                    left = new BinaryTree(valueList.Dequeue());
+
+                    node.Left = left;
+                    childrenList.Enqueue(left);
+                }
+
+                if (valueList.Count > 0)
+                {
+                    right = new BinaryTree(valueList.Dequeue());
+
+                    node.Right = right;
+                    childrenList.Enqueue(right);
+                }
+
+            }
+
+            if (valueList.Count > 0)
+            {
+                buildNodes(childrenList, valueList);
+            }
+
+        }
+
+        // Converts a string to a queue of integers, throws an error if input is not valid
+        private static Queue<int> convertStringToQueue(string input)
+        {
+            // Convert string to array
+            string[] strArray = input.Split(',');
+
+            // Create queue<int> to be populated
+            Queue<int> intList = new Queue<int>();
+
+            int parseValue;
+            foreach (string strVal in strArray)
+            {
+                // convert to int
+                bool parseSuccess = int.TryParse(strVal, out parseValue);
+                if (parseSuccess)
+                {
+                    intList.Enqueue(parseValue);
+                }
+                else
+                {
+                    throw new ArgumentException(strVal + " is not a valid integer.");
+                }
+            }
+
+            return intList;
         }
     }
 }
